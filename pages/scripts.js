e0d1db9ab2a6a9906f309dce12db7a25035b4d3d
Aldebaran Sanz <aldebaran@7fotografos.com>
2024-04-20 13:55:48 -0500

    refactorizar función de abrir y cerrar popups

--- pages/scripts.js
+++ pages/scripts.js
@@ -1,41 +1,42 @@
+//variables popups
 const popup = document.querySelector(".popup");
 const popupEditProfile = document.getElementById("popupEditProfile");
 const popupAddPlace = document.getElementById("popupAddPlace");
 const addButton = document.querySelector(".profile__add-button");
 const editButton = document.querySelector(".profile__edit-button");
-// const popupClose = document.querySelector(".popup__close-icon");
 const popupCloseEditProfile = document.getElementById(
   "popupEditProfile__close-icon"
 );
 const popupCloseAddPlace = document.getElementById("popupAddPlace__close-icon");
-const popupOverlay = document.querySelector(".popup__overlay");
+
+const inputName = document.querySelector("#name");
+const btnForm = document.querySelector("#saveBtn");
+const profileName = document.querySelector(".profile__name");
 
 // Funciones
 
 function addPlace() {
   console.log("click en añadir lugar");
   popupAddPlace.classList.add("popup__opened");
-  popupOverlay.classList.add("popup__opened");
 }
 
 function editProfile() {
   console.log("click en editar perfil");
   popupEditProfile.classList.add("popup__opened");
-  popupOverlay.classList.add("popup__opened");
 }
 
-function closePopup() {
+function closePopup(popupToClose) {
   console.log("click en cerrar popup");
-  // popup.classList.remove("popup__opened");
-  // popupOverlay.classList.remove("popup__opened");
-  popupEditProfile.classList.remove("popup__opened");
-  popupAddPlace.classList.remove("popup__opened");
-  popupOverlay.classList.remove("popup__opened");
+  popupToClose.classList.remove("popup__opened");
 }
 
 // Eventos - Listeners
 
 addButton.addEventListener("click", addPlace);
 editButton.addEventListener("click", editProfile);
-popupCloseEditProfile.addEventListener("click", closePopup);
-popupCloseAddPlace.addEventListener("click", closePopup);
+popupCloseEditProfile.addEventListener("click", function () {
+  closePopup(popupEditProfile);
+});
+popupCloseAddPlace.addEventListener("click", function () {
+  closePopup(popupAddPlace);
+});
