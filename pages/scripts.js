3fa3266f875f376c47294b751031ee806867c7cd
Aldebaran Sanz <aldebaran@7fotografos.com>
2024-04-19 20:37:19 -0500

    implementar funcion de abrir y cerrar popups de editar perfil y añadir lugar

--- pages/scripts.js
+++ pages/scripts.js
@@ -1,17 +1,41 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const addButton = document.querySelector(".profile__add-button");
-  const popup = document.querySelector(".popup");
-  const editButton = document.querySelector(".profile__edit-button");
+const popup = document.querySelector(".popup");
+const popupEditProfile = document.getElementById("popupEditProfile");
+const popupAddPlace = document.getElementById("popupAddPlace");
+const addButton = document.querySelector(".profile__add-button");
+const editButton = document.querySelector(".profile__edit-button");
+// const popupClose = document.querySelector(".popup__close-icon");
+const popupCloseEditProfile = document.getElementById(
+  "popupEditProfile__close-icon"
+);
+const popupCloseAddPlace = document.getElementById("popupAddPlace__close-icon");
+const popupOverlay = document.querySelector(".popup__overlay");
 
-  function addPlace() {
-    console.log("click en añadir lugar");
-    addButton.classList.add("popup_opened");
-  }
+// Funciones
 
-  function editProfile() {
-    console.log("click en editar perfil");
-  }
+function addPlace() {
+  console.log("click en añadir lugar");
+  popupAddPlace.classList.add("popup__opened");
+  popupOverlay.classList.add("popup__opened");
+}
 
-  addButton.addEventListener("click", addPlace);
-  editButton.addEventListener("click", editProfile);
-});
+function editProfile() {
+  console.log("click en editar perfil");
+  popupEditProfile.classList.add("popup__opened");
+  popupOverlay.classList.add("popup__opened");
+}
+
+function closePopup() {
+  console.log("click en cerrar popup");
+  // popup.classList.remove("popup__opened");
+  // popupOverlay.classList.remove("popup__opened");
+  popupEditProfile.classList.remove("popup__opened");
+  popupAddPlace.classList.remove("popup__opened");
+  popupOverlay.classList.remove("popup__opened");
+}
+
+// Eventos - Listeners
+
+addButton.addEventListener("click", addPlace);
+editButton.addEventListener("click", editProfile);
+popupCloseEditProfile.addEventListener("click", closePopup);
+popupCloseAddPlace.addEventListener("click", closePopup);
