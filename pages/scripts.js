1eae5830b140d056564f263296de5e9176d047c3
Aldebaran Sanz <aldebaran@7fotografos.com>
2024-11-21 15:59:59 -0500

    feat: agregar funciones de añadir lugar, like y borrar imagen

--- pages/scripts.js
+++ pages/scripts.js
@@ -17,11 +17,81 @@ const inputAboutMe = document.querySelector("#aboutMe");
 const aboutMe = document.querySelector(".profile__role");
 const inputTitle = document.querySelector("#title");
 const inputLink = document.querySelector("#link");
+const formAddCard = document.querySelector(".popup__form_addcard");
 
 //variables tarjetas
-const cardContainer = document.querySelector(".elements__element");
+const cardTemplate = document
+  .getElementById("card-template")
+  .content.querySelector(".elements__element");
+const cardContainer = document.querySelector(".elements__grid");
 
-// FUNCIONES
+const initialCards = [
+  {
+    name: "Valle de Yosemite",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/yosemite.jpg",
+  },
+  {
+    name: "Lago Louise",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/lake-louise.jpg",
+  },
+  {
+    name: "Montañas Calvas",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/bald-mountains.jpg",
+  },
+  {
+    name: "Latemar",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/latemar.jpg",
+  },
+  {
+    name: "Parque Nacional de la Vanoise",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/vanoise.jpg",
+  },
+  {
+    name: "Lago di Braies",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/lago.jpg",
+  },
+];
+
+// Función para crear una tarjeta a partir de los datos
+function createCard(name, link) {
+  const card = cardTemplate.cloneNode(true); // Clonar el template
+
+  // Modificar los elementos con los datos de la tarjeta
+  card.querySelector(".elements__title").textContent = name;
+  card.querySelector(".elements__image").src = link;
+  card.querySelector(".elements__image").alt = `Imagen de ${name}`;
+
+  //card.style.backgroundImage = `url(${link})`;
+  card
+    .querySelector(".elements__like-icon")
+    .addEventListener("click", function () {
+      card
+        .querySelector(".elements__like-icon")
+        .classList.toggle("elements__like-icon_active");
+    });
+
+  card
+    .querySelector(".elements__trash-icon")
+    .addEventListener("click", function () {
+      card.remove();
+    });
+
+  /*
+    card.queruSelector('.elements__image').addEventListener('click' , function() {
+      //abrir popup
+       popupImage.classList.add("popup__opened");
+      popupImageTitle.textContent = name;
+      popupImageImg.src = link;
+    })
+    */
+  return card;
+}
+
+// Añadir las tarjetas al cargar la página
+initialCards.forEach(({ name, link }) => {
+  const newCard = createCard(name, link);
+  cardContainer.appendChild(newCard); // Insertar cada tarjeta en el contenedor
+});
 
 //Añadir Lugar
 
@@ -83,3 +153,16 @@ popupCloseAddPlace.addEventListener("click", function () {
   resetAddplaceForm();
 });
 saveButton.addEventListener("click", saveProfile);
+
+formAddCard.addEventListener("submit", function (event) {
+  event.preventDefault();
+  const name = inputTitle.value;
+  const link = inputLink.value;
+  if (name.trim() === "" || link.trim() === "") {
+    alert("Por favor, completa todos los campos.");
+    return;
+  }
+  const card = createCard(name, link);
+  cardContainer.prepend(card);
+  closePopup(popupAddPlace);
+});
